ee59558ce7a29074a8fe3efdba7f535b5760bdb2
Soumyadeep Ghoshal <soumyadeep.ghoshal@oracle.com>
2016-12-03 19:45:24 -0800

    Use explicit namespace, switch to on_status and add README.txt.
    - on_data is an overkill for now as it returns more data than required currently.

--- twitter_stream.py
+++ twitter_stream.py
@@ -1,14 +1,11 @@
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+import tweepy
 import json
 import config
 
-class TwitterListener(StreamListener):
+class TwitterListener(tweepy.StreamListener):
 
-    def on_data(self, data):
-        print data
-        # print json.dumps(data, sort_keys=True,indent=4,separators=(',', ': '))
+    def on_status(self, status):
+        print status
         return True
 
     def on_error(self, status):
@@ -17,9 +14,9 @@ class TwitterListener(StreamListener):
 if __name__ == '__main__':
     twitter_listener = TwitterListener()
 
-    auth = OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
-    stream = Stream(auth, twitter_listener)
+    stream = tweepy.streaming.Stream(auth, twitter_listener)
 
     stream.filter(track=['python', 'javascript', 'ruby'])
